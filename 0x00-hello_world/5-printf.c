60a4a6987d992324ad9f7380134024887472b61a
Lost-Kode <leefrancis4040@gmail.com>
2022-06-02 18:44:25 +0000

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * Main -Print a string
+ * Description: use printf
+ * Reture: Alway return(0)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	reture(0);
+}
