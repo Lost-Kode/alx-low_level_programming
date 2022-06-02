015befbaa82947721fac139462023fd5d3545b1b
Lost-Kode <leefrancis4040@gmail.com>
2022-06-02 21:36:30 +0000

    size is not grandeur

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,6 +10,6 @@ int main(void)
 	printf("%lu\n", sizeof(char));
 	printf("%lu\n", sizeof(int));
 	printf("%lu\n", sizeof(float));
-	printf("%lu", sizeof(double));
+	printf("%lu\n", sizeof(double));
 	return (0);
 }
