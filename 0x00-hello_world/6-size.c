bd1d933c12496b9d3777986aaf8d8b7c3626e64a
Lost-Kode <leefrancis4040@gmail.com>
2022-06-02 21:32:11 +0000

    Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,18 +4,12 @@
  * main - Enty point
  * Return: Always 0 (Success)
  */
-
+#include <stdio.h>
 int main(void)
 {
-	char acharacter;
-	int ainteger;
-	long along;
-	long long alonglong;
-	float afloat;
-	printf("Size of a char: %lu byte(s)\n", sizeof(acharacter));
-	printf("Size of an int: %lu byte(s)\n", sizeof(ainteger));
-	printf("Size of a long int: %lu byte(s)\n", sizeof(along));
-	printf("Size of a long long int: %lu byte(s)\n", sizeof(alonglong));
-	printf("Size of a float: %lu byte(s)\n", sizeof(afloat));
+	printf("%lu\n", sizeof(char));
+	printf("%lu\n", sizeof(int));
+	printf("%lu\n", sizeof(float));
+	printf("%lu", sizeof(double));
 	return (0);
 }
