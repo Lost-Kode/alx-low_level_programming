e25f6ac4ef30cab9b8a9dc4885188867f87ab8c5
Lost-Kode <leefrancis4040@gmail.com>
2022-06-07 15:21:37 +0000

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main -> print  _putchar in this function
+ *
+ * Return: always to 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+
+	putchar('\n')
+	return (0);
+}
