325101655c6e27124958d27d0c4204ce6051976b
Lost-Kode <leefrancis4040@gmail.com>
2022-06-08 13:14:02 +0000

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers and returns the result.
+ *
+ * @x: number one.
+ * @i: number two.
+ *
+ * Return: Add of number one and number two.
+ */
+int add(int x, int i)
+{
+		return (x + i);
+}
