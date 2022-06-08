9c8e6d44ff4dcd740860560130e51a7a56b2737f
Lost-Kode <leefrancis4040@gmail.com>
2022-06-08 12:51:31 +0000

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Prints the minutes of a day
+ *
+ * Return: no return
+ */
+void jack_bauer(void)
+{
+	int k, o, d, e;
+
+	for (k = 48; k <= 50; k++)
+	{
+
+		for (o = 48; o <= 57; o++)
+		{
+			for (d = 48; d <= 53; d++)
+			{
+				for (e = 48; e <= 57; e++)
+				{
+					if (a  >= 50 && b >= 52)
+						break;
+					_putchar(k)
+					_putchar(o);
+					_putchar(58);
+					_putchar(d);
+					_putchar(e);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
