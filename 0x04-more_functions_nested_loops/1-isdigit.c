cc63ba46d04c22f1fd592bece64ecd4cb20c8dd7
Lost-Kode <leefrancis4040@gmail.com>
2022-06-09 14:55:06 +0000

    isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -7,10 +7,6 @@
  * Return: Return 1 if it is a digit otherwise return 0.
  */
 int _isdigit(int c)
-
-	if (c >= 48 && c <= 57)
-	{
-	return (1);
-	}
-	return (0);
+{
+	return (c >= '0' && c <= '9');
 }
