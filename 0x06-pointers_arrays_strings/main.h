313991b9ed048bdd59303c0038a6c22d013145d1
Lost-Kode <leefrancis4040@gmail.com>
2022-06-16 19:13:21 +0000

    101-print_number.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,6 +2,9 @@
 #define MAIN_H
 
 char *_strcat(char *dest, char *src);
+void print_number(int n);
+int _putchar(char c);
+void print_buffer(char *b, int size);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
@@ -9,5 +12,7 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
+char *rot13(char *);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 #endif /* MAIN_H */
