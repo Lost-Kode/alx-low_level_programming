a1b1cf5adcbc62a8bc724798eb61d1d086ecce21
Lost-Kode <realuncledrew2020@gmail.com>
2022-06-22 09:24:23 -0700

    task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - counts string length
+ * @s: string to be counted
+ * Return: number of char
+ */
+
+
+
+int _strlen_recursion(char *s)
+
+{
+
+	if (*s == '\0')
+
+	{
+
+		return (0);
+
+	}
+
+	s++;
+
+
+
+	return (1 + _strlen_recursion(s));
+
+}
