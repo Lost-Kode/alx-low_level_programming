90f7b0424583b4045ee1cf347c4132dbad3475c3
Lost-Kode <leefrancis4040@gmail.com>
2022-06-29 03:30:21 +0000

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,40 +1,55 @@
 #include "main.h"
-#include <stdlib.h>
-#include <stdio.h>
-
 /**
- * argstostr - concatenate all arguments of your program with newline
- * @ac: argument count
- * @av: double pointer to array of strings passed to main
- * Return: Null if fail, else return pointer to new string
+ * argstostr - a function that concatenates all the arguments of your program
+ * @ac: count of args passed to the function
+ * @av:array of arguments
+ *
+ * Return: pointer to the new string
  */
 
 char *argstostr(int ac, char **av)
 {
-	char *a, *p;
-	int i, x, t;
+	char *new_string = NULL;
+	int k = 0, i = ac, j, sum = 0, temp = 0;
 
-	if (!ac || !av)
+	if (ac == 0 || av == NULL)
 		return (NULL);
-	for (i = 0, t = 0; i < ac; i++)
+	while (ac--)
+		sum += (len(av[ac]) + 1);
+	new_string = (char *) malloc(sum + 1);
+	if (new_string != NULL)
 	{
-	for (x = 0; *(*(av + i) + x) != '\0'; x++, t++);
-	}
-	t++;
-	a = malloc(t * sizeof(char));
-	if (!a)
-		return (NULL);
-	p = a;
-	for (i = 0; i < ac; i++)
-	{
-		for (x = 0; av[i][x] != '\0'; x++)
+		while (k < i)
 		{
-			*a = av[i][x];
-			a++;
+			for (j = 0; av[k][j] != '\0'; j++)
+				new_string[j + temp] = av[k][j];
+			new_string[temp + j] = '\n';
+			temp += (j + 1);
+			k++;
 		}
-		*a = '\n';
-		a++;
+		new_string[temp] = '\0';
 	}
-	return (p);
+	else
+	{
+		return (NULL);
+	}
+	return (new_string);
+}
+
+/**
+ * len - returns length of str
+ * @str: string counted
+ * Return: returns the length
+ */
+int len(char *str)
+{
+	int len = 0;
+
+	if (str != NULL)
+	{
+		while (str[len])
+			len++;
+	}
+	return (len);
 }
 
