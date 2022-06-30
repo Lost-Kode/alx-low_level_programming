c0e1120ce98da374eba39f139857f5794d284a38
Lost-Kode <leefrancis4040@gmail.com>
2022-06-30 22:44:32 +0000

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,30 +9,30 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+		unsigned int i;
 
-	for (i = 0; i < n; i++)
-	{
-	s[i] = b;
-	}
-	return (s);
+		for (i = 0; i < n; i++)
+		{
+			s[i] = b;
+		}
+		return (s);
 }
 
 /**
- *__calloc - allocates memory for an array using malloc
+ * _calloc - allocates memory for an array using malloc
  * @nmemb: n elements
  * @size: bytes
  * Return: pointer
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *x;
+	void *p;
 
-	if (!nmemb || !size)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
-	x = malloc(nmemb * size);
-	if (!x)
+	p = malloc(nmemb * size);
+	if (p == NULL)
 		return (NULL);
-	_memset(x, 0, (nmemb * size));
-	return (x);
+	_memset(p, 0, (nmemb * size));
+	return (p);
 }
