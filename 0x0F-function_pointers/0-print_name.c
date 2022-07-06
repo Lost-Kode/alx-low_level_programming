2603929e56822101fafe40319e80be9ab735a53c
Lost-Kode <realuncledrew2020@gmail.com>
2022-07-07 00:56:08 +0100

    C - Function pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,12 @@
 #include "function_pointers.h"
+#include "function_pointers.h"
 
 /**
- * print_name - Prints a name.
- * @name: The name to be printed.
- * @f: A function that prints a name.
+ * print_name - prints a name
+ * @name: name's main
+ * @f: void function
  */
+
 void print_name(char *name, void (*f)(char *))
 {
 	if (name && f)
